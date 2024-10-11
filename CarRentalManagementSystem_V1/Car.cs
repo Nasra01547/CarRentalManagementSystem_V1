8b550156defe0db6891ba808541610e81c063eb3
UT01547 <UT01547@UNICOMTIC1>
2024-10-11 14:07:05 +0530

    Add project files.

---
+++ CarRentalManagementSystem_V1/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem_V1
+{
+    internal class Car
+    {
+        private string carId {  get; set; }
+        private string brand {  get; set; }
+        private string model { get; set; }
+        private decimal rentalPrice {  get; set; }
+
+        public Car(string CarId, string Brand, string Model, decimal RentalPrice)
+        {
+            carId = CarId;
+            brand = Brand;
+            model = Model;
+            rentalPrice = RentalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {carId}, Brand: {brand}, Model: {model}, RentalPrice: {rentalPrice}";
+        }
+
+
+    }
+}
---
+++ CarRentalManagementSystem_V1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalManagementSystem_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("CAR_001", "TOYOTA", "PRIUS", 10);
+            Console.WriteLine(car1);
+        }
+    }
+}
